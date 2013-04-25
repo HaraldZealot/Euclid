5d2b306f74faca937f353ec36d76b408acb76a3d
Harald Zealot <harald_zealot@tut.by>
2013-04-25 03:11:26 +0300

    gcd and lcm

--- main.d
+++ main.d
@@ -2,5 +2,25 @@ import std.stdio;
 
 void main()
 {
-   writeln("Hello сусветік :)");
+   long a,b;
+   readf(" %s %s",&a,&b);
+   writeln("gcd(",a,", ",b,") = ",gcd(a,b));
+   writeln("lcm(",a,", ",b,") = ",lcm(a,b));
+}
+
+T gcd(T)(T a, T b)
+{
+   if(!b)return a;
+   while(a%b)
+   {
+      auto c=a%b;
+      a=b;
+      b=c;
+   };
+   return b;
+}
+
+long lcm(long a, long b)
+{
+   return a*(b/gcd(a,b));
 }
