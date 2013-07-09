f7e5f46a8f59d2a7b85a381db46cf7ae816ccdc0
HaraldZealot <harald_zealot@tut.by>
2013-07-09 22:09:10 +0300

    mantissa processing 1 phase

--- main.d
+++ main.d
@@ -9,14 +9,14 @@ void main()
    writeln("gcd(",a,", ",b,") = ",gcd(a,b));
    writeln("lcm(",a,", ",b,") = ",lcm(a,b)); */
    writeln(0.5," = ", ChainFraction!int(0.5));
-   writeln(1.5," = ", ChainFraction!int(1.5));
+   writeln(1.5," = ", ChainFraction!int(1.5e17));
    writeln(1.6," = ", ChainFraction!int(1.6));
    writeln(0.3," = ", ChainFraction!int(0.3));
-   writeln(0.333," = ", ChainFraction!int(0.333));
+   writeln(0.33," = ", ChainFraction!long(0.33));
    writeln(0.3333333333333333333333," = ", ChainFraction!int(0.33333333333333333333333));
-   writeln(SQRT2," = ", ChainFraction!byte(SQRT2));
-   writeln(SQRT1_2," = ", ChainFraction!int(SQRT1_2));
-   writeln(PI," = ", ChainFraction!long(PI));
+   //writeln(SQRT2," = ", ChainFraction!byte(SQRT2));
+   //writeln(SQRT1_2," = ", ChainFraction!int(SQRT1_2));
+   //writeln(PI," = ", ChainFraction!long(PI));
 }
 
 T gcd(T)(T a, T b)
@@ -40,24 +40,45 @@ struct ChainFraction(T)
 {
     this(real number)
     {
-	auto x = floor(number);
-	integral_=cast(T) x;
-	x = number - x;
-	T pPre = 1,
-	    pCur = integral_,
+	writefln("\n\nnumber = %.20g\nnumber = %.20a",number,number);
+	int expn;
+	auto mantissa = frexp(number, expn);
+	ulong mnt = cast(ulong) ldexp(mantissa,64);
+	writefln("mnt = %.20a", mantissa);
+	writefln("mnt =    %x",mnt);
+	writeln("exp = ", expn);
+	mnt>>=1;
+	writefln("mnt =    %x",mnt);
+	ulong base = 1UL << 63;
+	writefln("bas =    %x",base);
+	ulong gcdValue = gcd(base,mnt);
+	ulong Q = base / gcdValue;
+	ulong P = mnt  / gcdValue;
+	writefln("P =    %x",P);
+	writefln("Q =    %x",Q);
+	T   pPre = 1,
+	    pCur = 0,
 	    qPre = 0,
-	    qCur = 1,
-	    count = 0;
-	while(abs(x)>float.epsilon && qCur<T.max/3)
+	    qCur = 1;
+	ulong a=1;
+	ulong b;
+	
+	
+	do
 	{
 	    
-	    x = 1.0/x;
-	    T a = cast(T) floor(x);
-	    /*writeln("a ", a);
+	    ulong aPre=a;
+	    a = Q / P;
+	    b = Q % P;
+	    Q = P;
+	    P = b;
+	    writefln("a = %d,\tb = %d", a, b);
+	    //*
 	    writeln("pPre ", pPre);
 	    writeln("pCur ", pCur);
 	    writeln("qPre ", qPre);
-	    writeln("qCur ", qCur);*/
+	    writeln("qCur ", qCur);//*/
+	    if(a/aPre>1000 || qCur>=(T.max-qPre)/a) break;
 	    auto temp = pCur;
 	    pCur = cast(T) (a * pCur + pPre);
 	    pPre = temp;
@@ -65,15 +86,17 @@ struct ChainFraction(T)
 	    temp = qCur;
 	    qCur = cast(T) (a * qCur + qPre);
 	    qPre = temp;
-	    x-=a;
 	    
-	    ++count;
+	    
+	  //  writeln("x ",x);
 	}
-	/*writeln("pPre ", pPre);
+	while(b);
+	/*
+	writeln("pPre ", pPre);
 	    writeln("pCur ", pCur);
 	    writeln("qPre ", qPre);
-	    writeln("qCur ", qCur);*/
-	numerator_ = cast(T) (pCur - integral_ * qCur);
+	    writeln("qCur ", qCur);//*/
+	numerator_ = pCur;
 	denominanator_ = qCur;    
     }
     
